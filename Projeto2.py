a41ae895afa0372be9b7ce8887d4603b178a9724
Alex Henriques <alexhenriques417@gmail.com>
2025-05-14 22:22:37 -0300

    Conversos de bases.
    Transforma os números decimais em binários e octais.

---
+++ Projeto2.py
@@ -0,0 +1,11 @@
+#conversor de decimal para binário
+numero = int(input("Digite um número inteiro para transformar decimal para binário: "))
+print("{} convertido para binário é igual a {}".format(numero, bin(numero)[2:]))
+
+#conversor de decimal para octal
+numero = int(input("Digite um número inteiro para transformar decimal para octal: "))
+print("{} convertido para octal é igual a {}". format(numero, oct(numero)[2:]))
+
+
+
+   
